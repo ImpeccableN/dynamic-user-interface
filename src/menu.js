f7ce61aaaa52a3f38e8081c530a2497b391b13ab
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2025-05-12 16:41:35 +0200

    added some styles to the site

--- src/menu.js
+++ src/menu.js
@@ -1,36 +1,35 @@
 const menuButtons = Array.from(document.getElementsByClassName("menuButton"));
 
-
-
-const hideMenu = function(parent) {
-    const menuNodes = Array.from(parent.children);
-    menuNodes.forEach(element => {
-        element.setAttribute("style", "display: none");
-    });
+const hideMenu = function (parent) {
+  const menuNodes = Array.from(parent.children);
+  menuNodes.forEach((element) => {
+    element.setAttribute("style", "display: none");
+  });
 };
 
-const showMenu = function(parent) {
-    const menuNodes = Array.from(parent.children);
-    menuNodes.forEach(element => {
-        element.setAttribute("style", "display: block");
-    });
+const showMenu = function (parent) {
+  const menuNodes = Array.from(parent.children);
+  menuNodes.forEach((element) => {
+    element.setAttribute("style", "display: block");
+  });
 };
 
-const buttonFunction = function(button) {
-    let menuHidden = true;
-    button.addEventListener("click", () => {
-        if (menuHidden) {
-            showMenu(button);
-            menuHidden = false;
-        } else {
-            hideMenu(button);
-            menuHidden = true;
-        }
-    });
+const buttonFunction = function (button) {
+  let menuHidden = true;
+  button.addEventListener("click", (event) => {
+    if (button !== event.target) return;
+    if (menuHidden) {
+      showMenu(button);
+      menuHidden = false;
+    } else {
+      hideMenu(button);
+      menuHidden = true;
+    }
+  });
 };
 
-export const init = function() {
-    menuButtons.forEach(hideMenu);
-    menuButtons.forEach(buttonFunction);
-    // buttonFunction();
+export const init = function () {
+  menuButtons.forEach(hideMenu);
+  menuButtons.forEach(buttonFunction);
+  // buttonFunction();
 };
